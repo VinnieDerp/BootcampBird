51ad5502e53dec52149b3d0f33ddc140621f2074
Vincent Muller <vincent.muller03@gmail.com>
2025-09-15 15:06:28 +0200

    The game starts with already some obstacles, and they now spawn in further away

--- Assets/Scripts/Handlers/ObstacleSpawner.cs
+++ Assets/Scripts/Handlers/ObstacleSpawner.cs
@@ -13,6 +13,15 @@ public class ObstacleSpawner : MonoBehaviour
     {
         timer = timerLength / 2;
         spawnedObstacles.Clear();
+
+        for (int i = 0; i < 6; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), transform.position.z - i * timerLength * obstacleSpeed);
+            Quaternion randomRotation = new Quaternion(0, 0, 100, Random.Range(0, 360));
+            GameObject spawnedObstacle = Instantiate(obstaclePrefab, randomPosition, randomRotation, transform);
+            spawnedObstacles.Add(spawnedObstacle);
+            Debug.Log("Starting obstacles spawned");
+        }
     }
 
     // Update is called once per frame
@@ -25,14 +34,14 @@ public class ObstacleSpawner : MonoBehaviour
             Quaternion randomRotation = new Quaternion(0, 0, 100, Random.Range(0, 360));
             GameObject spawnedObstacle = Instantiate(obstaclePrefab, randomPosition, randomRotation, transform);
             spawnedObstacles.Add(spawnedObstacle);
-            Debug.Log("obstacle spawned");
+            Debug.Log("Obstacle spawned");
             timer = 0;
         }
 
         foreach (GameObject obstacle in spawnedObstacles)
         {
             obstacle.transform.position -= new Vector3(0, 0, obstacleSpeed * Time.deltaTime);
-            if (obstacle.transform.position.z < -2)
+            if (obstacle.transform.position.z < -1)
             {
                 spawnedObstacles.Remove(obstacle);
                 Destroy(obstacle);
