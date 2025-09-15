220c729415fc4156a2adabaa75705f23a3b4811b
Vincent Muller <vincent.muller03@gmail.com>
2025-09-15 14:44:15 +0200

    Added an obstacle spawner script for creating, moving and destroying obstacles

---
+++ Assets/Scripts/Handlers/ObstacleSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public List<GameObject> spawnedObstacles = new List<GameObject>();
+    public float obstacleSpeed;
+    public float timerLength;
+    private float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timer = timerLength / 2;
+        spawnedObstacles.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timerLength)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), transform.position.z);
+            Quaternion randomRotation = new Quaternion(0, 0, 100, Random.Range(0, 360));
+            GameObject spawnedObstacle = Instantiate(obstaclePrefab, randomPosition, randomRotation, transform);
+            spawnedObstacles.Add(spawnedObstacle);
+            Debug.Log("obstacle spawned");
+            timer = 0;
+        }
+
+        foreach (GameObject obstacle in spawnedObstacles)
+        {
+            obstacle.transform.position -= new Vector3(0, 0, obstacleSpeed * Time.deltaTime);
+            if (obstacle.transform.position.z < -2)
+            {
+                spawnedObstacles.Remove(obstacle);
+                Destroy(obstacle);
+            }
+        }
+    }
+}
